c6e5010cdf0a5c5f4d5a90dc763cdbd87a457717
pochhammer <leo.poch.hammer@gmail.com>
2015-01-05 21:45:03 +0100

    15/01/05

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cat input.txt | sort > midput.txt
+# grep "kat" midput.txt > src/s.tex
+grep "katalgebra" midput.txt > src/sAlgebra.tex
+grep "katanaliza" midput.txt > src/sAnaliza.tex
+grep "katfunkcjonalna" midput.txt > src/sFunkcjonalna.tex
+grep "katgeometria" midput.txt > src/sGeometria.tex
+grep "kathmh" midput.txt > src/sHmh.tex
+grep "katliczby" midput.txt > src/sLiczby.tex
+grep "katliniowa" midput.txt > src/sLiniowa.tex
+grep "katmiara" midput.txt > src/sMiara.tex
+grep "katnumeryka" midput.txt > src/sNumeryka.tex
+grep "kattopologia" midput.txt > src/sTopologia.tex
+grep "katzbiory" midput.txt > src/sZbiory.tex
+rm midput.txt
